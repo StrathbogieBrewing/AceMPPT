1a81bd4946d73a72c69b51b3d67fbcedc1ea4f4c
Johny <johnyhoops@gmail.com>
2021-09-07 14:51:21 +1000

    Batch commit

--- examples/AceMPPT/AceMPPT.ino
+++ examples/AceMPPT/AceMPPT.ino
@@ -1,12 +1,11 @@
 #include "AceMPPT.h"
-
 #include "AceBMS.h"
 #include "AceBus.h"
 #include "VEDirect.h"
 
-
 #define kRxInterruptPin (19)
-AceBus aceBus(Serial1, kRxInterruptPin);
+void aceCallback(tinframe_t *frame);
+AceBus aceBus(Serial1, kRxInterruptPin, aceCallback);
 
 void mppt2Callback(uint16_t id, int32_t value);
 void mppt3Callback(uint16_t id, int32_t value);
@@ -48,33 +47,29 @@ void loop() {
   if(millis() % 1000 == 0) {
     mppt2.ping();
     mppt3.ping();
-    // tinframe_t txFrame;
-    // msg_t *txMsg = (msg_t *)txFrame.data;
-    // sig_encode(txMsg, ACEMPPT_VPV2, panelVoltage2);
-    // sig_encode(txMsg, ACEMPPT_ICH2, chargeCurrent2);
-    // sig_encode(txMsg, ACEMPPT_VPV3, panelVoltage3);
-    // sig_encode(txMsg, ACEMPPT_ICH3, chargeCurrent3);
-    // if(aceBus.write(&txFrame) == AceBus_kOK){
-    //   hexDump("TX ", (unsigned char*)&txFrame, tinframe_kFrameSize);
-    // } else {
-    //   Serial.println("TX Error");
-    // }
-
   }
 
   int status = aceBus.update();
   if (status == AceBus_kWriteCollision) Serial.println("TX Collision");
   if (status == AceBus_kWriteTimeout) Serial.println("TX Timeout");
-  if (status == AceBus_kWriteComplete) Serial.println("TX Complete");
-  if (status == AceBus_kReadDataReady) {
-    tinframe_t rxFrame;
-    aceBus.read(&rxFrame);
+  // if (status == AceBus_kWriteComplete) Serial.println("TX Complete");
+
+}
+
+void aceCallback(tinframe_t *rxFrame){
+  // if (status == AceBus_kReadDataReady) {
+  //   tinframe_t rxFrame;
+  //   aceBus.read(&rxFrame);
     // hexDump("RX ", (unsigned char*)&rxFrame, tinframe_kFrameSize);
-    msg_t *msg = (msg_t *)(rxFrame.data);
+    msg_t *msg = (msg_t *)(rxFrame->data);
     int16_t value;
     if (sig_decode(msg, ACEBMS_VBAT, &value) != FMT_NULL) {
       uint32_t senseVoltage = (value + 5) / 10 - 10;
-      mppt2.set(VEDirect_kBatterySense, senseVoltage);
+      int16_t balance = (int16_t)chargeCurrent2 - (int16_t)chargeCurrent3;
+      if(balance > 64) balance = 64;
+      if(balance < -64) balance = -64;
+      balance /= 16;
+      mppt2.set(VEDirect_kBatterySense, senseVoltage + balance);
       mppt3.set(VEDirect_kBatterySense, senseVoltage);
     }
     if (sig_decode(msg, ACEBMS_RQST, &value) != FMT_NULL) {
@@ -88,15 +83,15 @@ void loop() {
           sig_encode(txMsg, ACEMPPT_VPV3, panelVoltage3);
           sig_encode(txMsg, ACEMPPT_ICH3, chargeCurrent3);
           if(aceBus.write(&txFrame) == AceBus_kOK){
-            hexDump("TX ", (unsigned char*)&txFrame, tinframe_kFrameSize);
+            // hexDump("TX ", (unsigned char*)&txFrame, tinframe_kFrameSize);
           } else {
             Serial.println("TX Error");
           }
 
         }
+      }
       // }
-    }
-  }
+    // }
 }
 
 void mppt2Callback(uint16_t id, int32_t value) {
