115201e102bc2e6d11a1b81ad075de496f7ddd6a
Johny <johnyhoops@gmail.com>
2021-09-20 18:21:04 +1000

    Batch commit

--- examples/AceMPPT/AceMPPT.ino
+++ examples/AceMPPT/AceMPPT.ino
@@ -1,6 +1,6 @@
-#include "AceMPPT.h"
 #include "AceBMS.h"
 #include "AceBus.h"
+#include "AceMPPT.h"
 #include "VEDirect.h"
 
 #define kRxInterruptPin (19)
@@ -35,87 +35,71 @@ void setup() {
   mppt3.begin();
   aceBus.begin();
   mppt2.ping();
-
-  // hexDump("TST", (uint8_t*)&testFrame, tinframe_kFrameSize);
-  // Serial.println(tinframe_checkFrame(&testFrame));
 }
 
 void loop() {
   mppt2.update();
   mppt3.update();
 
-  if(millis() % 1000 == 0) {
+  if (millis() % 1000 == 0) {
     mppt2.ping();
     mppt3.ping();
   }
 
   int status = aceBus.update();
-  if (status == AceBus_kWriteCollision) Serial.println("TX Collision");
-  if (status == AceBus_kWriteTimeout) Serial.println("TX Timeout");
+  if (status == AceBus_kWriteCollision)
+    Serial.println("TX Collision");
+  if (status == AceBus_kWriteTimeout)
+    Serial.println("TX Timeout");
   // if (status == AceBus_kWriteComplete) Serial.println("TX Complete");
-
 }
 
-void aceCallback(tinframe_t *rxFrame){
+void aceCallback(tinframe_t *rxFrame) {
   // if (status == AceBus_kReadDataReady) {
   //   tinframe_t rxFrame;
   //   aceBus.read(&rxFrame);
-    // hexDump("RX ", (unsigned char*)&rxFrame, tinframe_kFrameSize);
-    msg_t *msg = (msg_t *)(rxFrame->data);
-    int16_t value;
-    if (sig_decode(msg, ACEBMS_VBAT, &value) != FMT_NULL) {
-      uint32_t senseVoltage = (value + 5) / 10 - 10;
-      int16_t balance = (int16_t)chargeCurrent2 - (int16_t)chargeCurrent3;
-      if(balance > 64) balance = 64;
-      if(balance < -64) balance = -64;
-      balance /= 16;
-      mppt2.set(VEDirect_kBatterySense, senseVoltage + balance);
-      mppt3.set(VEDirect_kBatterySense, senseVoltage);
+  // hexDump("RX ", (unsigned char*)&rxFrame, tinframe_kFrameSize);
+  msg_t *msg = (msg_t *)(rxFrame->data);
+  int16_t value;
+  if (sig_decode(msg, ACEBMS_VBAT, &value) != FMT_NULL) {
+    uint32_t senseVoltage = (value + 5) / 10 - 5;
+    int16_t balance = (int16_t)chargeCurrent2 - (int16_t)chargeCurrent3;
+    if (balance > 64)
+      balance = 64;
+    if (balance < -64)
+      balance = -64;
+    balance /= 16;
+    mppt2.set(VEDirect_kBatterySense, senseVoltage + balance);
+    mppt3.set(VEDirect_kBatterySense, senseVoltage);
+  }
+  if (sig_decode(msg, ACEBMS_RQST, &value) != FMT_NULL) {
+    uint8_t frameSequence = value;
+    if ((frameSequence & 0x03) == 0x03) {
+      tinframe_t txFrame;
+      msg_t *txMsg = (msg_t *)txFrame.data;
+      sig_encode(txMsg, ACEMPPT_VPV2, panelVoltage2);
+      sig_encode(txMsg, ACEMPPT_ICH2, chargeCurrent2);
+      sig_encode(txMsg, ACEMPPT_VPV3, panelVoltage3);
+      sig_encode(txMsg, ACEMPPT_ICH3, chargeCurrent3);
+      aceBus.write(&txFrame);
     }
-    if (sig_decode(msg, ACEBMS_RQST, &value) != FMT_NULL) {
-      // if(panelVoltage2 && chargeCurrent2 && panelVoltage3 && chargeCurrent3){
-        uint8_t frameSequence = value;
-        if((frameSequence & 0x03) == 0x03){ //}(SIG_MSG_ID(ACEMPPT_STATUS) & 0xFF)){
-          tinframe_t txFrame;
-          msg_t *txMsg = (msg_t *)txFrame.data;
-          sig_encode(txMsg, ACEMPPT_VPV2, panelVoltage2);
-          sig_encode(txMsg, ACEMPPT_ICH2, chargeCurrent2);
-          sig_encode(txMsg, ACEMPPT_VPV3, panelVoltage3);
-          sig_encode(txMsg, ACEMPPT_ICH3, chargeCurrent3);
-          if(aceBus.write(&txFrame) == AceBus_kOK){
-            // hexDump("TX ", (unsigned char*)&txFrame, tinframe_kFrameSize);
-          } else {
-            Serial.println("TX Error");
-          }
-
-        }
-      }
-      // }
-    // }
+  }
 }
 
 void mppt2Callback(uint16_t id, int32_t value) {
-  if (id == VEDirect_kPanelVoltage){
+  if (id == VEDirect_kPanelVoltage) {
     panelVoltage2 = value;
-    Serial.print(F("Vp2 : "));
-    Serial.println(value * 0.01);
   }
-  if (id == VEDirect_kChargeCurrent){
+  if (id == VEDirect_kChargeCurrent) {
     chargeCurrent2 = value;
-    Serial.print(F("Ic2 : "));
-    Serial.println(value * 0.1);
   }
 }
 
 void mppt3Callback(uint16_t id, int32_t value) {
-  if (id == VEDirect_kPanelVoltage){
+  if (id == VEDirect_kPanelVoltage) {
     panelVoltage3 = value;
-    Serial.print(F("Vp3 : "));
-    Serial.println(value * 0.01);
   }
-  if (id == VEDirect_kChargeCurrent){
+  if (id == VEDirect_kChargeCurrent) {
     chargeCurrent3 = value;
-    Serial.print(F("Ic3 : "));
-    Serial.println(value * 0.1);
   }
 }
